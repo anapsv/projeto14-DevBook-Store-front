755b6ce5e580f7000bd32466550cf42877fb473d
brualvess <oliveirabrunna2210@gmail.com>
2022-07-06 20:31:16 -0300

    layout login

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,93 @@
-
+import styled from "styled-components";
+import topo from './assets/img/topo.png'
 export default function Login() {
     return (
         <>
-        <p>Página com campos para fazer login, com um link embaixo de "não tem conta, crie" - vai direcionar pra página de signup</p>
+            <ImgContainer>
+                <img src={topo} alt='DevBook-Store' />
+            </ImgContainer>
+            <Logar><h1>Login</h1></Logar>
+            <Form>
+            <input type='email' placeholder="E-mail" />
+            <input type='password' placeholder="Senha"/>
+            <Botao>
+                <Entrar>Entrar</Entrar>
+            </Botao>
+            <Cadastro >Primeira vez ? Cadastre-se!</Cadastro>
+        </Form>
         </>
     );
 }
+const ImgContainer = styled.div`
+    width: 100%;
+    height: 120%;
+    background-color: #9cd8e2;
+    display: flex;
+    justify-content: center;
+  ;
+
+    img {
+        width: 306px;
+        height: 160px;
+        
+    }
+
+`
+const Logar = styled.div`
+    display: flex;
+    align-self: center;
+    justify-content: center;
+    h1{
+        font-family: 'Raleway';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 40px;
+    margin-top : 80px
+
+    }
+`
+const Form = styled.form`
+display: flex;
+justify-content: center;
+flex-direction: column;
+margin-top: 50px;
+  input{
+width: 30%;
+height: 50px;
+align-self:center;
+border-radius: 5px;
+background: #FFFFFF;
+border: 1px solid #D5D5D5;
+font-family: 'Lexend Deca';
+font-style: normal;
+font-weight: 400;
+font-size: 19.976px;
+margin-top: 15px;
+  }
+  `
+const Botao = styled.div`
+background: #D3D3D3;
+border-radius: 5px;
+width: 30%;
+height: 50px;
+margin-top: 30px;
+align-self: center;
+display: flex;
+justify-content: center;
+`
+const Entrar = styled.div`
+font-family: 'Raleway';
+font-style: normal;
+font-weight: 700;
+font-size: 20px;
+color:#FFFFFF;
+margin-top: 12px;
+`
+const Cadastro = styled.div`
+font-family: 'Raleway';
+font-style: normal;
+font-weight: 700;
+font-size: 18px;
+text-align: center;
+color:#000000;
+margin-top: 50px`
